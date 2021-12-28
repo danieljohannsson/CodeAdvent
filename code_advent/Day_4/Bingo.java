471e2284a10671cd6cc880380e6423917b43b084
danieljohannsson <daniel.johannsson1999@gmail.com>
2021-12-29 00:54:20 +0100

    Completed first problem of Day_4

---
+++ code_advent/Day_4/Bingo.java
@@ -0,0 +1,75 @@
+package code_advent.Day_4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bingo {
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("code_advent/Day_4/input.txt"));
+        List<String> nbrs = new ArrayList<>(Arrays.asList(sc.next().split(",")));
+        System.out.println(nbrs);
+
+        List<List<List<String>>> bingo = new ArrayList<>();
+
+        while(sc.hasNext()){
+            List<List<String>> board = new ArrayList<>();
+            for(int i = 0; i < 5; i++){
+                List<String> row = new ArrayList<>();
+                for(int j = 0; j < 5; j++){
+                    if(sc.hasNext()) row.add(sc.next());
+                }
+                board.add(row);
+            }
+            bingo.add(board);
+        }
+        boolean found = false;
+        int count = 0;
+        List<List<String>> winner = new ArrayList<>();
+
+        while(!found){
+
+                for(List<List<String>> board: bingo){
+                    
+                    for(int i = 0; i < 5; i++){
+                        for(int j = 0; j < 5; j++){
+                            if(board.get(i).get(j).equals(nbrs.get(count))){
+                                board.get(i).set(j, "X");
+                                if(isRowBingo(board.get(i)) || isColBingo(board)){
+                                    found = true;
+                                    winner = board;
+                                    System.out.println(nbrs.get(count));
+                                }
+                            }
+                        }
+                    }
+                }
+            if(!found) count++;    
+        }
+        System.out.println(nbrs.get(count));
+        System.out.println(winner);
+    }
+
+    public static boolean isRowBingo(List<String> arr){
+        for(String s: arr){
+            if(!s.equals("X")) return false;
+        }
+        return true;
+    }
+
+    public static boolean isColBingo(List<List<String>> matrix){
+        for(int i = 0; i < 5; i++){
+            List<String> list = new ArrayList<>();
+            for(int j = 0; j < 5; j++){
+                list.add(matrix.get(j).get(i));
+            }
+            if(isRowBingo(list)) return true;
+        }
+        return false;
+    }
+
+}
